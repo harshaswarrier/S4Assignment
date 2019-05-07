739bb4de0518d87ec9e6b56d38abad519cfc6bf9
swathythadukkassery <35335341+swathythadukkassery@users.noreply.github.com>
2019-05-07 12:55:57 +0530

    Kiranniya Madhu(40), Fathima N K (27)

---
+++ scientificCalculator/Answer_Button.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		Scanner p= new Scanner(System.in);
+		String alpha = myObj.nextLine();
+		int n=p.nextInt();
+		alpha=Integer.toString(n);
+	    System.out.println(alpha);
+	}
+}
