999bd2a5f0be8d8950491454bdc5473e4170cf1d
ArathyUday <35325573+ArathyUday@users.noreply.github.com>
2019-05-08 07:29:38 +0530

    01. Aadil Abdul Latheef K P , 16. Arathy Uday

---
+++ scientificCalculator/InverseTrig.java
@@ -0,0 +1,53 @@
+package scientificCalculator;
+import java.lang.Math.*;
+ 
+class InverseTrig {
+ 
+
+        public static void sininv(double a){
+	 
+        System.out.println(Math.asin(a)*180/Math.PI);          //Math.asin returns in radian, so it is converted into degree
+        }
+
+
+        public static void cosinv(double b){
+        System.out.println(Math.acos(b)*180/Math.PI);          //Math.acos returns in radian, so it is converted into degree
+        }
+
+
+        public static void taninv(double c){
+        System.out.println(Math.atan(c)*180/Math.PI);          //Math.atan returns in radian, so it is converted into degree
+    }
+    public static void main(String args[])
+    { 
+       double d= Double.parseDouble(args[1]);
+        if (args[0].equals("arcsin"))
+         { 
+           if (d <= -1 && d >= 1)                             //domain of sineinverse is -1 to 1
+               {
+                 System.out.println("Invalid Input");
+                }
+            else 
+		{ sininv(d);}
+          
+        }
+
+      
+
+       if (args[0].equals("arccos"))
+         {
+            if (d < -1 && d > 1)                              //domain of cosinverse is -1 to 1
+               {
+                 System.out.println("Invalid Input");
+                }
+            else
+          cosinv(d);
+        }
+     
+
+        if (args[0].equals("arctan"))
+         {
+          taninv(d);                                         //domain of taninverse is -infinite to infinite, hence no conditions needed
+        }    
+    }
+}
